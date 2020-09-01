45f882774810a2847bcd131457dd15a4a8056771
Changyoon-Lee <lckddbsl@gmail.com>
2020-09-01 13:52:10 +0900

    0901 알고리즘 연습

--- algorithm/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 4/3. 뮤직비디오/AA.py
+++ algorithm/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 4/3. 뮤직비디오/AA.py
@@ -1,19 +1,19 @@
 import sys
-sys.stdin = open('섹션 4/3. 뮤직비디오/in5.txt')
+sys.stdin = open('섹션 4/3. 뮤직비디오/in6.txt')
 
 n_song, n_dvd = map(int, input().split())
 song_list = list(map(int, input().split()))
 
 upper = max(song_list)*(n_song//n_dvd+1)
 search = upper//2
-lower = 0
+lower = max(song_list)
 
 while True:
     temp = 0
     cnt = 1 #DVD의 갯수 counting 1부터 시작
     for song in song_list:
         temp+=song
-        if temp > search: #제한시간 넘어가면 다음 DVD로 시작
+        if temp > search : #제한시간 넘어가면 다음 DVD로 시작
             cnt+=1
             temp = song
 
