9291e4d87979c65b4ad97460a8b99e4bd6c37daa
Changyoon-Lee <lkingkongl@naver.com>
2020-09-11 01:07:25 +0900

    지역변수 전역변수

--- algorithm/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 2/2. K번째 작은수/AA.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin=open('input.txt','rt')
-T=int(input())
-for i in range(T):
-    n, s, e, k=map(int, input().split())
-    a=list(map(int, input().split()))
-    a=a[s-1:e]
-    a.sort()
-    print('#{} {}'.format(i+1,a[k-1]))
--- algorithm/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/3. 카드 역배치/AA.py
+++
@@ -1,9 +0,0 @@
-import sys
-# sys.stdin = open('섹션 3/3. 카드 역배치/in1.txt')
-num_list = list(range(1,21,1))
-
-for i in range(10):
-    a,b = map(int,input().split())
-    num_list[a-1:b] = num_list[a-1:b][::-1]
-
-print(' '.join(list(map(str,num_list))))
